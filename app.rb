2de639ba7532db1ca71c7a8d331f49ae188540c1
Lukas_Skywalker <lukas.diener@hotmail.com>
2020-12-14 17:44:46 +0100

    add demos

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 require "sinatra/activerecord"
+require 'sinatra/cors'
 require_relative 'score'
+
+set :allow_origin, "*"
+set :allow_methods, "*"
+set :allow_headers, "*"
+set :expose_headers, "*"
 
 post '/score' do
   Score.create(game: params[:game], name: params[:name], score: params[:score])
